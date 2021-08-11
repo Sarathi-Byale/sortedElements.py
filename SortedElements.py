b3a4e27578db5d41008ec033dc0edbe6c88d3e61
Sarathi <54929256+Sarathi-Byale@users.noreply.github.com>
2021-08-11 10:31:36 +0530

    Update SortedElements.py

--- SortedElements.py
+++ SortedElements.py
@@ -1,12 +1,12 @@
+print("Enter the input")
 n=input()
+c=[]
 z=set(n)
-x=[]
-z=list(sorted(z))
+z=list(z)
+print("Sorted elments are as follow:")
 for i in range(len(z)):
   co=0
   for m in range(len(n)):
     if n[m]==z[i]:
-      co = co+1;
-  x.append(co)
-r=sorted(x)
-print(r)
+      co = co+1
+  print(str(z[i])+"="+str(co))
